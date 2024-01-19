e1354cf68e29da89d3c4d8d1018d3fc779f93c1b
IoannaNika <nana11892@gmail.com>
2024-01-19 18:07:52 +0100

    update make template to find amplicons based on primers located on each strain

--- make_template_from_fasta.py
+++ make_template_from_fasta.py
@@ -5,42 +5,83 @@ import os
 import sys
 import argparse
 from Bio import SeqIO
+import subprocess
 
-def create_template(seq, id):
-    template = "data/nCoV-2019.bed"
+def parse_R_output(output):
+    # parse output from R script
+    output = output.split("\n")
+    start = output[1].split(" ")[1]
+    end = output[4].split(" ")[1]
+    return start, end
+
+def get_amplicon_positions(Fprob, Rprob, seq_path):
+    # call r script to get amplicon positions
+    results = subprocess.run(["Rscript match_primers.R " + Fprob + " " + Rprob + " " + seq_path], shell=True, capture_output=True, text=True)
+    results = parse_R_output(results.stdout)
+    start = int(results[0]) + len(Fprob)
+    end = int(results[1]) - len(Rprob)
+    return start, end
+
+def get_primer_sequences(primer_file, index):
+
+    # read primer file
+    primer_df = pd.read_csv(primer_file, sep='\t', header=0)
+    primer_df.columns = ["name", "pool", "seq", "length", "%gc", "tm (use 65)"]
+
+    # get primer sequences
+    Fprob = primer_df.iloc[index]["seq"]
+    Rprob = primer_df.iloc[index+1]["seq"]
+    
+    return Fprob, Rprob
+
+
+def create_template(seq_path, id, template, primer_file):
     template_df = pd.read_csv(template, sep='\t', header=None)
     template_df.columns = ["chr", "start", "end", "name_1", "name_2", "strand"]
-    final_template = ""
+    final_template = "" 
+
     # read template file two lines at a time (positive & negative strand info)
     for i in range(0, len(template_df), 2):
         cnt = 0
-        # get positive strand info
+        # get positive strand info for reference genome
         pos_strand = template_df.iloc[i]
-        # get negative strand info
+        # get negative strand info for reference genome
         neg_strand = template_df.iloc[i+1]
 
         seq_start = pos_strand["end"]
         seq_end = neg_strand["start"]
+        try:
+            # get primer sequences
+            Fprob, Rprob = get_primer_sequences(primer_file, i)
 
-        if seq_start > len(seq):
-            break
+            # get amplicon positions
+            start, end = get_amplicon_positions(Fprob, Rprob, seq_path)
 
-        if seq_end > len(seq):
-            seq_end = len(seq)
+        except:
+            print("Something went wrong with the amplicon positions.")
+            continue
+
+        # parse the fasta file
+        record = SeqIO.parse(seq_path, "fasta")
+        seq = next(record).seq
         
-        amplicon = seq[seq_start:seq_end]
+        amplicon = seq[start:end]
 
         while cnt < 10:
             final_template += ">" + id + ":" + str(seq_start) + "_" + str(seq_end) + ":" + str(cnt) + "\n"
             final_template += str(amplicon) + "\n"
             cnt += 1
-    
+
     return final_template
+
 
 
 def main():
     parser = argparse.ArgumentParser(description="Creates amplicon fragments")
     parser.add_argument('--dir', dest = 'dir', required=True, type=str, help="path to data directory")
+    parser.add_argument('--bed_file', dest = 'bed_file', required=False, default="data/nCoV-2019.bed", type=str, help="path to bed file with primer locations")
+    parser.add_argument('--primer_file', dest = 'primer_file', required=False, default="data/nCoV-2019.tsv",type=str, help="path to primer file")
+
     args = parser.parse_args()
 
     # get directories in data directory
@@ -62,12 +103,11 @@ def main():
         for file in files:
             file_path = directory + "/" + file
             gisaid_id = file.split(".")[0]
-            # parse the fasta file
-            record = SeqIO.parse(file_path, "fasta")
+            
+            seq_path = directory + "/" + gisaid_id + ".fasta"
+           
 
-            seq = next(record).seq
-
-            template = create_template(seq, gisaid_id)
+            template = create_template(seq_path, gisaid_id, args.bed_file, args.primer_file)
             # write template to file
             template_file = directory + "/" + gisaid_id + ".template"
 
---
+++ match_primers.R
@@ -0,0 +1,19 @@
+library(Biostrings)
+library(stringr) 
+library(seqinr)
+
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)!= 3) 
+   stop("Three arguments must be supplied: Fprobe, Rprobe and the sequence", call.=FALSE)[1]
+
+seqfile <-args[3]
+sequence <- read.fasta(seqfile, as.string = TRUE, seqonly = TRUE)[[1]]
+Fprobe <- args[1]
+Rprobe <- args[2]
+sequence <-DNAString(str_replace_all(sequence, "[\r\n]", ""))
+rg <- ranges(matchProbePair(Fprobe, Rprobe, sequence, algorithm="auto", logfile=NULL, verbose=FALSE))
+result_list <- list(start(rg), end(rg))
+result_list
+ 
